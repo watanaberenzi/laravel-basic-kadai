570fd83dfec3a33c10887aecb6e8d232d7ef94ef
watanaberenzi <tender_fake@yahoo.co.jp>
2024-08-08 14:42:52 +0900

    投稿ページ表示の課題

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+ // ルーティングを設定するコントローラを宣言する
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostsController::class, 'index']);
